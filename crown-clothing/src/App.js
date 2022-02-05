16151591116ca918ccfdeb788087e05f5b8c3733
Susmita <susmitapatil87@gmail.com>
2022-02-05 12:03:28 +0530

    updating our app with shop page,collection preview component and collection item component

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
+import ShopPage from "./pages/shop/shop.component";
 
 import { Route, Routes } from "react-router-dom";
 
-const HatsPage = () => (
-  <div>
-    <h1>hats page</h1>
-  </div>
-);
+// const HatsPage = () => (
+//   <div>
+//     <h1>hats page</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
-        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
