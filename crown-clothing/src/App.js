a04c0c21e006f46b3e44fc508948b6036e0b2691
Susmita <susmitapatil87@gmail.com>
2022-02-05 09:40:32 +0530

    update our menu items to navigate to their pages

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
 
+import { Route, Routes } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>hats page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- crown-clothing/src/components/menu-item/menu-item.components.jsx
+++ crown-clothing/src/components/menu-item/menu-item.components.jsx
@@ -1,17 +1,42 @@
 import React from "react";
+// import { withRouter } from "react-router-dom";
+// import { withRouter } from "react-router";
 import "./menu-item.styles.scss";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+// function withRouter(Component) {
+//   function ComponentWithRouterProp(props) {
+//     let location = useLocation();
+//     let navigate = useNavigate();
+//     let params = useParams();
+
+//     return <Component {...props} router={{ location, navigate, params }} />;
+//   }
+
+//   return ComponentWithRouterProp;
+// }
+
+let navigate;
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
+  (navigate = useNavigate()),
+  (
     <div
-      className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    ></div>
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={() => {
+        navigate(`/${linkUrl}`);
+        // history.push(`/${linkUrl}`);
+      }}
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
+  )
 );
 
 export default MenuItem;
