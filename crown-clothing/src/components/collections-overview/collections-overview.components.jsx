868e0877b3e686cb88e368da6b79ca26cf21bf1f
Susmita <susmitapatil87@gmail.com>
2022-02-26 23:12:25 +0530

    Added directory and collections state into redux, and added collection overview component

---
+++ crown-clothing/src/components/collections-overview/collections-overview.components.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./collections-overview.styles.scss";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import CollectionPreview from "../../components/collection-preview/collection-preview.components";
+
+const CollectionOverview = ({ collections }) => (
+  <div className="collection-overview">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
