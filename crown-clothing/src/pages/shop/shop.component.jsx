dfd95bc0af2c3094254a712dbaf678f3cfaa8633
Susmita <susmitapatil87@gmail.com>
2022-03-06 12:46:55 +0530

    Replaced onSnapshot method with promises

--- crown-clothing/src/pages/shop/shop.component.jsx
+++ crown-clothing/src/pages/shop/shop.component.jsx
@@ -25,7 +25,8 @@ class ShopPage extends Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
-    collectionRef.onSnapshot(async (snapshot) => {
+
+    collectionRef.get().then((snapshot) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
